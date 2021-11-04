18b09c4a08ef01a0bdb3f12e2171974b5c1066b6
蘇建豪 <jenhausu@osensetech.com>
2021-11-04 17:46:06 +0800

    Initial Commit

---
+++ ObjectCaptureModeling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ObjectCaptureModeling
+//
+//  Created by Mac mini on 2021/11/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ObjectCaptureModeling/ObjectCaptureModelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectCaptureModelingApp.swift
+//  ObjectCaptureModeling
+//
+//  Created by Mac mini on 2021/11/4.
+//
+
+import SwiftUI
+
+@main
+struct ObjectCaptureModelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
